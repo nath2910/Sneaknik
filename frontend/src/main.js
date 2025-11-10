97ffbe510a0b68d476b5489baa43980a93f025bf
nathan talvasson <nathantalvasson@gmail.com>
2025-11-11 00:21:28 +0100

    css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,4 +4,6 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
+console.log('VITE_API_URL =', import.meta.env.VITE_API_URL)
+
 createApp(App).use(router).mount('#app')
