dba80781adfffe1272561e460ce37f5c54188b64
nathan talvasson <nathantalvasson@gmail.com>
2025-12-28 17:06:00 +0100

    up

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { useAuthStore } from '@/store/authStore'
+import { useAuthStore } from '@/store/AuthStore'
 
 // Lazy-load des pages
 const HomePage = () => import('@/pages/homePage.vue')
